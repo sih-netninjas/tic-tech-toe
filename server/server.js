313f43a4d07c5b654327b21c6513bca62d09610c
devansh6620 <79559686+djman323@users.noreply.github.com>
2024-09-28 03:51:53 +0530

    temp

--- server/server.js
+++ server/server.js
@@ -1,20 +1,22 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const connectDB = require('./config/mongodb')
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectDB = require("./config/mongodb");
 
-const app = express()
-const PORT = 5000
+const app = express();
+const PORT = 5000;
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-connectDB()
+connectDB();
 
-app.get('/', (req, res) => {
-  res.send('Hello')
-})
+app.use("/subjects", require("./routes/subject.routes"));
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost${PORT}`)
-})
+  console.log(`Server is running on port http://localhost${PORT}`);
+});
